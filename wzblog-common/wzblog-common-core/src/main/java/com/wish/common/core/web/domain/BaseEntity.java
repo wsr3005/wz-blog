ab9442d901ce8a9ffe262098a79d58698f56465f
wyq <wishmri@gmail.com>
2020-12-02 20:04:33 +0800

    Entity基类

---
+++ wzblog-common/wzblog-common-core/src/main/java/com/wish/common/core/web/domain/BaseEntity.java
@@ -0,0 +1,55 @@
+package com.wish.common.core.web.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @program: wz-blog
+ * @description: Entity基类
+ * @author: Ylover
+ * @created: 2020/12/02 17:50
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /** 搜索值 */
+    private String searchValue;
+
+    /** 创建者 */
+    private String createBy;
+
+    /** 创建时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    /** 更新者 */
+    private String updateBy;
+
+    /** 更新时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+
+    /** 备注 */
+    private String remark;
+
+    /** 开始时间 */
+    @JsonIgnore
+    private String beginTime;
+
+    /** 结束时间 */
+    @JsonIgnore
+    private String endTime;
+
+    /** 请求参数 */
+    private Map<String, Object> params;
+}
